646b0a19ee217a00ac612e4823310786b2cbe373
marcosludwig <marcosludwig@gmail.com>
2024-04-27 20:06:03 -0300

    Melhorias de design e escolha de data

--- js/script.js
+++ js/script.js
@@ -1,13 +1,36 @@
+// carrega o datepicker com a data de hoje.
+var data = new Date();
+var dia = String(data.getDate()).padStart(2, '0');
+var mes = String(data.getMonth() + 1).padStart(2, '0');
+var ano = data.getFullYear();
+
+if (typeof myDate !== 'undefined')
+  myDate.value = ano + '-' + mes + '-' + dia;
+
 const apiUrl = 'https://liturgiadiaria.site';
 
-fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
-    insertDataIntoHTML(data);
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
+let bIsIndex = false;
+if (window.location.pathname.endsWith('index.html')) {
+  localStorage.clear();
+  bIsIndex = true;
+}
+
+var strData = localStorage.getItem('dataLiturgiaDiaria');
+
+if (bIsIndex || strData === null || strData === "") {
+
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      insertDataIntoHTML(data);
+      localStorage.setItem('dataLiturgiaDiaria', JSON.stringify(data));
+    })
+    .catch(error => { console.error('Error fetching data:', error); });
+}
+else {
+  var dataLiturgiaDiaria = JSON.parse(strData);
+  insertDataIntoHTML(dataLiturgiaDiaria);
+}
 
 function insertDataIntoHTML(data) {
   const dataKeys = Object.keys(data);
@@ -19,6 +42,9 @@ function insertDataIntoHTML(data) {
     if (sectionElement) {
       if (key === 'data') {
         sectionElement.innerHTML = `<h2>Liturgia do dia ${textContent}</h2>`;
+      }
+      if (key === 'liturgia') {
+        sectionElement.innerHTML = `<p>${textContent}</p>`;
       }
       else if (key === 'cor') {
         sectionElement.innerHTML = `<p>Cor litúrgica: ${textContent}</p>`;
@@ -59,6 +85,10 @@ function insertDataIntoHTML(data) {
 
         if (texto === undefined) {
           /* se não há segunda leitura, deixa vazio*/
+          sectionElement.innerHTML = `
+              <h2>Segunda leitura</h2>
+              <p>Não há segunda leitura!</p>
+            `;
         }
         else {
           const referencia = data.primeiraLeitura.referencia;
@@ -83,3 +113,43 @@ function insertDataIntoHTML(data) {
     }
   }
 }
+
+function changeDate()
+{
+  var x = document.getElementById("myDate").value;
+  if (x === '')
+  {
+    alert("Insira uma data válida");
+    return;
+  }
+
+  var dataNew = new Date(x);
+  // Ajuste para o fuso horário local
+  dataNew.setMinutes(dataNew.getMinutes() + dataNew.getTimezoneOffset());
+
+  var dayNew = String(dataNew.getDate()).padStart(2, '0');
+  var monthNew = String(dataNew.getMonth() + 1).padStart(2, '0');
+  
+  const apiUrlByDate = apiUrl + '/' + dayNew + '-' +  monthNew;
+
+  fetch(apiUrlByDate)
+    .then(response => response.json())
+    .then(data => {
+      if (typeof data.erro !== 'undefined') {
+        if (data.erro != "") {
+          alert("Erro retornado pela API: " + data.erro);
+        }
+      }
+      else if (typeof data.aviso === 'undefined') {
+        alert("Servidor retornou uma estrutura desconhecida"); 
+      }
+      else if (data.aviso !== "") {
+        insertDataIntoHTML(data);
+        localStorage.setItem('dataLiturgiaDiaria', JSON.stringify(data));
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+      alert("Servidor retornou erro");
+    });
+} 
